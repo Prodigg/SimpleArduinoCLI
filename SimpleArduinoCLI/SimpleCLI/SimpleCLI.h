293a715c43fa66c14a30e52dc059e71af00dc779
Prodigg <raphael@familie-sauer.ch>
2024-09-05 20:29:01 +0200

    added "better" LICENCE

--- SimpleArduinoCLI/SimpleCLI/SimpleCLI.h
+++ SimpleArduinoCLI/SimpleCLI/SimpleCLI.h
@@ -1,13 +1,25 @@
 /*
-* Licence:
-* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-* OTHER DEALINGS IN THE SOFTWARE.
+MIT License
+
+Copyright (c) 2024 Prodigg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 #ifndef __SIMPLE_CLI_H__
@@ -148,10 +160,8 @@ private:
 	void(*executeEveryCycle)() = NULL;	// ptr to CycleFunction
 	bool executeEveryCycleActive = false; // if true executes CycleFunction
 
-#ifndef SIMPLE_CLI_NO_DYNAMIC_MEM_ALLOC
 	bool* ActiveCLIOptions = NULL;	// ptr to array that store what CLI options are active
 	uint32_t ActiveCLIOptionsLength = 0;
-#endif // !SIMPLE_CLI_NO_DYNAMIC_MEM_ALLOC
 
 	bool areGreetingsEN = true;
 
